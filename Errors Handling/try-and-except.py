ca25e070e7d5548fdf763b0926567178b2cb1145
Singh.Rajiv@bcg.com <Singh.Rajiv@bcg.com>
2021-11-29 15:02:20 -0800

    Try except code

---
+++ Errors Handling/try-and-except.py
@@ -0,0 +1,9 @@
+try:
+    # Want to attempt this code
+    # May have an error
+    result = 10 + '10'
+except:
+    print('The error happened due to type mismatch')
+else:
+    print('Add went well')
+    print(result)
