838420314ebd04f645d97f204ef63cbef80016cd
Singh.Rajiv@bcg.com <Singh.Rajiv@bcg.com>
2021-12-01 19:30:02 -0800

    Basic fundamentals of web scraping

---
+++ Web Scraping/web-scraping.py
@@ -0,0 +1,26 @@
+"""
+RULES OF WEB SCRAPING
+- Always try to get permission to scrape, otherwise your IP address may get blocked
+- A slight change or update to a website may completely break your web scraping script
+- Pretty much every web scraping project is a unique and custom job
+"""
+
+"""
+MAIN FRONT END COMPONENTS OF A WEBSITE
+- HTML: to create the basic structure and content of a webpage
+- CSS: used for the design and style of a webpage (where to place and how it looks)
+- JS: used to define the interactive elements of a webpage
+"""
+
+"""
+LIBRARIES TO USE FOR WEB SCRAPING USING PYTHON (Install them using conda or pip)
+- BeautifulSoap
+- Requests
+"""
+
+"""
+INSTALLING THE NECESSARY LIBRARIES
+pip install requests
+pip install lxml
+pip install bs4
+"""
