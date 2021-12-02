e20b84ba9ff1abe3d066c3fc208564641c42a091
Singh.Rajiv@bcg.com <Singh.Rajiv@bcg.com>
2021-12-02 13:44:27 -0800

    Example when the number of pages is unknown

--- Web Scraping/example2-quotes.py
+++ Web Scraping/example2-quotes.py
@@ -35,7 +35,7 @@ for tag in soup.select(".tag-item"):
 print(top_ten)
 
 
-# Task 7: All the authors in the website
+# Task 7: All the authors in the website if the number of pages are known
 
 base_url = "http://quotes.toscrape.com/page/"
 authors = set()
@@ -48,5 +48,36 @@ for page in range(1, 10):
 
     for name in soup.select('.author'):
         authors.add(name.text)
+
+print(authors)
+
+
+# Task 8: All the authors in the website if the number of pages are not known
+page_still_valid = True
+authors = set()
+page = 1
+
+while page_still_valid:
+
+    # Concatenate to get new page URL
+    page_url = base_url+str(page)
+    
+    # Obtain Request
+    res = requests.get(page_url)
+    
+    # Check to see if we're on the last page
+    if "No quotes found!" in res.text:
+        break
+    
+    # Turn into Soup
+    soup = bs4.BeautifulSoup(res.text,'lxml')
+    
+    # Add Authors to our set
+    for name in soup.select(".author"):
+        authors.add(name.text)
+        
+    # Go to Next Page
+    page += 1
+
 
 print(authors)
